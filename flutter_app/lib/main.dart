bb015f1c2a0f69001e466cfb7a1c54165e13e71f
Nam <sataponsf@gmail.com>
2025-02-28 14:40:10 +0700

    Update Student's Download
    Change webview to inappwebview
    Add download file in webview

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter_app/logic/drawer/drawer_bloc.dart';
 import 'package:flutter_app/pages/auth_page/login_page.dart';
 import 'package:flutter_app/widgets/main_wrapper.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await FlutterDownloader.initialize(
+      debug: true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl : true // option: set to false to disable working with http links (default: false)
+  );
+  
   runApp(
     MultiBlocProvider(
       providers: [
@@ -16,7 +25,6 @@ void main() {
     ),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
