e2b25eee2bbe687cbb9f310bca59f423a36d9a9e
Nam <sataponsf@gmail.com>
2025-03-09 20:59:52 +0700

    Fix signin
    ให้เลื่อนทั้งหน้า

--- flutter_app/lib/pages/auth_page/login_page.dart
+++ flutter_app/lib/pages/auth_page/login_page.dart
@@ -52,210 +52,222 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    
+
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Container(
-        child: Stack(
-          children: [
-            // Background gradient with wave shape
-            Container(
-              height: size.height,
-              width: size.width,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF8A56AC), // Deep purple
-                    Color(0xFFD47FA6), // Pink accent
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-            ),
-            
-            // Wave shape overlay
-            Positioned(
-              top: size.height * 0.2,
-              child: Container(
-                width: size.width,
-                height: size.height * 0.8,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40),
+        height: size.height,
+        width: size.width,
+        // Background gradient
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF8A56AC), // Deep purple
+              Color(0xFFD47FA6), // Pink accent
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              // Wave shape overlay
+              Positioned(
+                top: size.height * 0.2,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
                   ),
                 ),
               ),
-            ),
-            
-            // Content
-            SafeArea(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Header area 
-                    SizedBox(height: size.height * 0.05),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.lock_outline_rounded,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-        
-                          const SizedBox(height: 10),
-                          const Text(
-                            'Welcome back',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                          const SizedBox(height: 70,),
-        
-                        ],
-                      ),
-                    ),
-                    
-                    // Form area
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(30,10,30,30),
-                      width: size.width,
-                      child: Form(
-                        key: _formKey,
+
+              // Content
+              SafeArea(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Header area
+                      SizedBox(height: size.height * 0.05),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            // Username field
-                            _buildInputField(
-                              title: 'Username',
-                              controller: _usernameController,
-                              prefixIcon: Icons.person_outline_rounded,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter your username';
-                                }
-                                return null;
-                              },
+                            Icon(
+                              Icons.lock_outline_rounded,
+                              size: 50,
+                              color: Colors.white,
                             ),
-                            
-                            const SizedBox(height: 24),
-                            
-                            // Password field
-                            _buildInputField(
-                              title: 'Password',
-                              controller: _passwordController,
-                              prefixIcon: Icons.lock_outline_rounded,
-                              isPassword: true,
-                              isVisible: _isPasswordVisible,
-                              toggleVisibility: () {
-                                setState(() {
-                                  _isPasswordVisible = !_isPasswordVisible;
-                                });
-                              },
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter your password';
-                                }
-                                return null;
-                              },
-                            ),
-                            
-                            const SizedBox(height: 30),
-                            
-                            // Login button
-                            SizedBox(
-                              width: double.infinity,
-                              height: 55,
-                              child: ElevatedButton(
-                                onPressed: _isLoading 
-                                  ? null 
-                                  : () {
-                                    if (_formKey.currentState!.validate()) {
-                                      setState(() {
-                                        _isLoading = true;
-                                      });
-                                      loginUser().then((_) {
-                                        setState(() {
-                                          _isLoading = false;
-                                        });
-                                      });
-                                    }
-                                  },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xFF8A56AC),
-                                  foregroundColor: Colors.white,
-                                  elevation: 0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                ),
-                                child: _isLoading
-                                  ? const SizedBox(
-                                      width: 24,
-                                      height: 24,
-                                      child: CircularProgressIndicator(
-                                        color: Colors.white,
-                                        strokeWidth: 2,
-                                      ),
-                                    )
-                                  : const Text(
-                                      'Sign In',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
                               ),
                             ),
-                            
-                            const SizedBox(height: 20),
-                            
-                            // Sign up option
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  'Don\'t have an account?',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => SignUpPage(),
-                                      ),
-                                    );
-                                  },
-                                  child: const Text(
-                                    'Sign Up',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFF8A56AC),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                            const SizedBox(
+                              height: 70,
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
+
+                      // Form area
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
+                        width: size.width,
+                        constraints: BoxConstraints(
+                          minHeight:
+                              size.height * 0.8, // Ensure sufficient height
+                        ),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+                          ),
+                        ),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Username field
+                              _buildInputField(
+                                title: 'Username',
+                                controller: _usernameController,
+                                prefixIcon: Icons.person_outline_rounded,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter your username';
+                                  }
+                                  return null;
+                                },
+                              ),
+
+                              const SizedBox(height: 24),
+
+                              // Password field
+                              _buildInputField(
+                                title: 'Password',
+                                controller: _passwordController,
+                                prefixIcon: Icons.lock_outline_rounded,
+                                isPassword: true,
+                                isVisible: _isPasswordVisible,
+                                toggleVisibility: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter your password';
+                                  }
+                                  return null;
+                                },
+                              ),
+
+                              const SizedBox(height: 30),
+
+                              // Login button
+                              SizedBox(
+                                width: double.infinity,
+                                height: 55,
+                                child: ElevatedButton(
+                                  onPressed: _isLoading
+                                      ? null
+                                      : () {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            setState(() {
+                                              _isLoading = true;
+                                            });
+                                            loginUser().then((_) {
+                                              setState(() {
+                                                _isLoading = false;
+                                              });
+                                            });
+                                          }
+                                        },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xFF8A56AC),
+                                    foregroundColor: Colors.white,
+                                    elevation: 0,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                  ),
+                                  child: _isLoading
+                                      ? const SizedBox(
+                                          width: 24,
+                                          height: 24,
+                                          child: CircularProgressIndicator(
+                                            color: Colors.white,
+                                            strokeWidth: 2,
+                                          ),
+                                        )
+                                      : const Text(
+                                          'Sign In',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                ),
+                              ),
+
+                              const SizedBox(height: 20),
+
+                              // Sign up option
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Don\'t have an account?',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => SignUpPage(),
+                                        ),
+                                      );
+                                    },
+                                    child: const Text(
+                                      'Sign Up',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFF8A56AC),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -281,7 +293,6 @@ class _LoginPageState extends State<LoginPage> {
             color: Color(0xFF8A56AC),
           ),
         ),
-
         const SizedBox(height: 8),
         TextFormField(
           controller: controller,
@@ -351,7 +362,7 @@ class _LoginPageState extends State<LoginPage> {
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         String token = data['token'];
-        
+
         // Save the token in SharedPreferences
         logindata = await SharedPreferences.getInstance();
         logindata.setString('token', token);
@@ -364,7 +375,8 @@ class _LoginPageState extends State<LoginPage> {
         // Show error message
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('Login failed: ${jsonDecode(response.body)['message'] ?? 'Unknown error'}'),
+            content: Text(
+                'Login failed: ${jsonDecode(response.body)['message'] ?? 'Unknown error'}'),
             backgroundColor: Colors.red,
             behavior: SnackBarBehavior.floating,
           ),
