35c8b052c3106fb44e77d6f2d1b0cae0c178aa38
Nam <sataponsf@gmail.com>
2025-03-16 09:36:46 +0700

    Fix Url Course

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -6,7 +6,7 @@ const String COURSE_DATA = "http://202.44.40.179/Data_From_Chiab/json/courseData
 const String BACHELOR_CSB = "http://202.44.40.179/Data_From_Chiab/json/bachelor_bilingual.json";
 const String BACHELOR_NORMAL = "http://202.44.40.179/Data_From_Chiab/json/bachelor_normal.json";
 const String MASTER_CS = "http://202.44.40.179/Data_From_Chiab/json/master_cs.json";
-const String MASTER_SE = "http://202.44.40.179/Data_From_Chiab/json/master_se.jso";
+const String MASTER_SE = "http://202.44.40.179/Data_From_Chiab/json/master_se.json";
 const String PHD = "http://202.44.40.179/Data_From_Chiab/json/phd.json";
 
 // csb page
